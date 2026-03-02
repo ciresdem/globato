9808759aa966c7d418a15dbb4c4f6b90f76e405f
Matthew Love <matthew.love@colorado.edu>
2026-03-02 14:41:49 -0800

    raster.clip

--- src/globato/processors/rasters/clip.py
+++ src/globato/processors/rasters/clip.py
@@ -62,5 +62,5 @@ class RasterClipHook(RasterHook):
             # Set pixels OUTSIDE the polygons to nodata
             clipped_data = np.where(geom_mask, data, ndv)
 
-        logger.info(f"[Clip] clipped at {stage}.")
+        logger.info(f"[Clip] clipped at {self.stage}.")
         return clipped_data
